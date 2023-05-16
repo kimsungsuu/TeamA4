40911931468b7bacca81516ca6f5e22b02a01627
Kim sungsu <ksungsu>
2023-05-16 17:59:55 +0900

    A4 commit

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, jsonify, redirect
 app = Flask(__name__)
 
+import certifi
+import base64
+import gridfs
+
 from bson.objectid import ObjectId
 
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://sparta:test@cluster0.awfowzp.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
+
+fs = gridfs.GridFS(db)
+ca = certifi.where()
 
 @app.route('/')
 def home():
@@ -15,9 +22,19 @@ def home():
 @app.route("/api", methods=["GET"])
 def home_get():
    all_myself = list(db.myself.find({},{'_id':True, 'name':True, 'mbti':True, 'introduction':True, 
-                                         'strengths':True, 'collaboration_style':True ,'blog_url':True}))
+                                         'strengths':True, 'collaboration_style':True ,'blog_url':True
+                                         ,'github_url':True, 'photo':True}))
     
    for myself in all_myself:
+        photo_id = myself['photo']
+        if photo_id:
+           try:
+              image_data = fs.get(photo_id).read()
+              base64_img = base64.b64encode(image_data).decode('utf-8')
+              myself['photo'] = 'data:image/jpeg;base64,' + base64_img
+           except gridfs.error.NoFile as e:
+              print.error("파일이 없습니다.")  
+                 
         myself['_id'] = str(myself['_id'])
     
    return jsonify({'result':all_myself})
@@ -29,20 +46,26 @@ def get_write():
 
 @app.route("/api/new", methods=["POST"])
 def post_write():
+    photo_receive = request.files['photo_give']
     name_receive = request.form['name_give']
     mbti_receive = request.form['mbti_give']
     introduction_receive = request.form['introduction_give']
     strengths_receive = request.form['strengths_give']
     collaboration_style_receive = request.form['collaboration-style_give']
     blog_url_receive = request.form['blog-url_give']
+    github_receive = request.form['github-url_give']
+
+    file_img_id = fs.put(photo_receive)
     
     doc = {
+       'photo' : file_img_id,
        'name' : name_receive,
        'mbti' : mbti_receive,
        'introduction' : introduction_receive,
        'strengths' : strengths_receive,
        'collaboration_style' : collaboration_style_receive,
        'blog_url' : blog_url_receive,
+       'github_url' : github_receive
     }
     
     db.myself.insert_one(doc)
